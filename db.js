91fc215feb41305442ed622cc5a11fe4f773540e
lenartd <daniel.lenart@akademiasovy.sk>
2019-02-08 11:04:31 +0100

    fix

--- db.js
+++ db.js
@@ -16,9 +16,10 @@ module.exports = {
     writeData(event)
     {
       console.log("Writing to database");
-      let obj = JSON.parse(event);
-      
-      var data = "INSERT INTO weatherinfo (ArduinoID, Temperature, AirHumidity, SoilHumidity, WaterSurface, Date) VALUES (\'"+obj.identification.id+"\',\'"+ obj.info.temperature+"\',\'"+ obj.info.humidityAir+"\',\'"+ obj.info.humiditySoil+"\',\'"+ obj.info.watersurface+"\',\'"+ obj.date.date+"\')";
+      //let obj = JSON.parse(event);
+      let obj = event;
+
+      let data = "INSERT INTO weatherinfo (ArduinoID, Temperature, AirHumidity, SoilHumidity, WaterSurface, Date) VALUES (\'"+obj.identification.id+"\',\'"+ obj.info.temperature+"\',\'"+ obj.info.humidityAir+"\',\'"+ obj.info.humiditySoil+"\',\'"+ obj.info.watersurface+"\',\'"+ obj.date.date+"\')";
       con.query(data, function (err, result) {
         if (err) throw err;
         console.log("Record successfully inserted");
