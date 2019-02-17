b9edc337407eb981752f2ae392773b2aa2353f07
tomvarga620 <tomas.varga@akademiasovy.sk>
2019-02-17 20:38:44 +0100

    changed variables in index.script.js

--- web/js/index.script.js
+++ web/js/index.script.js
@@ -1,10 +1,24 @@
 // JavaScript Document
 
 $(document).ready(function() {
-	
-	/*const socket = () => { io('http://localhost:5485') };
-	//socket();*/
-	
+
+	//divs for display data from server
+	let stat1 = $('#stat1');
+	let stat2 = $('#stat1');
+	let stat3 = $('#stat1');
+	let stat4 = $('#stat1');
+
+	//div for dark mode onoffSwitch
+	let onoff = $('#onoffSwitch');
+	let colorChange = $('.colorChange');
+
+	//divs for dark mode
+	let heading = $('.heading');
+	let nameWrapper = $('#nameWrapper');
+	let middleWrapper = $('#middleWrapper');
+	let statsBox = $('.statsBox');
+	let chartWrapper = $('#chartWrapper');
+
     const socket = io.connect('http://localhost:5485');
 
 	socket.on('connect', (data) => {
@@ -18,10 +32,15 @@ $(document).ready(function() {
       let obj = JSON.parse(data);
       console.log(obj);
 
-		$('#stat1').text(obj[0].Temperature);
+		/*$('#stat1').text(obj[0].Temperature);
 		$('#stat2').text(obj[0].AirHumidity);
 		$('#stat3').text(obj[0].SoilHumidity);
-		$('#stat4').text(obj[0].WaterSurface);
+		$('#stat4').text(obj[0].WaterSurface);*/
+		
+		stat1.text(obj[0].Temperature);
+		stat2.text(obj[0].AirHumidity);
+		stat3.text(obj[0].SoilHumidity);
+		stat4.text(obj[0].WaterSurface);
 
 		Chart.defaults.global.maintainAspectRatio = false;
 		let chart = document.getElementById("chart").getContext("2d");
@@ -56,8 +75,19 @@ $(document).ready(function() {
     
     });
 	
+	onoff.click(() => {
 
-  	$('#onoffSwitch').click(() => {
+		colorChange.toggleClass('-darkmode');
+		heading.toggleClass('-darkmode');
+		nameWrapper.toggleClass('-darkmode');
+		middleWrapper.toggleClass('-darkmode');
+		statsBox.toggleClass('-darkmode');
+		chartWrapper.toggleClass('-darkmode');
+
+	});
+
+
+  	/*$('#onoffSwitch').click(() => {
 
   		$('.colorChange').toggleClass('-darkmode');
 		$('.heading').toggleClass('-darkmode');
@@ -66,7 +96,6 @@ $(document).ready(function() {
 	    $('.statsBox').toggleClass('-darkmode');
 		$('#chartWrapper').toggleClass('-darkmode');
 	
-	});
+	});*/
 
-	
 });
