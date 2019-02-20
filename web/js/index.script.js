d77e491e18407608d8d799e3b0f612ce3d4e4ce1
tomvarga620 <tomas.varga@akademiasovy.sk>
2019-02-20 01:23:59 +0100

    added functions to change chart on click

--- web/js/index.script.js
+++ web/js/index.script.js
@@ -4,9 +4,9 @@ $(document).ready(function() {
 
 	//divs for display data from server
 	let stat1 = $('#stat1');
-	let stat2 = $('#stat1');
-	let stat3 = $('#stat1');
-	let stat4 = $('#stat1');
+	let stat2 = $('#stat2');
+	let stat3 = $('#stat3');
+	let stat4 = $('#stat4');
 
 	//div for dark mode onoffSwitch
 	let onoff = $('#onoffSwitch');
@@ -16,8 +16,18 @@ $(document).ready(function() {
 	let heading = $('.heading');
 	let nameWrapper = $('#nameWrapper');
 	let middleWrapper = $('#middleWrapper');
-	let statsBox = $('.statsBox');
+	let statsBox1 = $('.statsBox1');
+	let statsBox2 = $('.statsBox2');
+	let statsBox3 = $('.statsBox3');
+	let statsBox4 = $('.statsBox4');
 	let chartWrapper = $('#chartWrapper');
+
+
+	//divs for charts
+	let divChart1 = $('.chart1');
+	let divChart2 = $('.chart2');
+	let divChart3 = $('.chart3');
+	let divChart4 = $('.chart4');
 
     const socket = io.connect('http://localhost:5485');
 
@@ -42,9 +52,43 @@ $(document).ready(function() {
 		stat3.text(obj[0].SoilHumidity);
 		stat4.text(obj[0].WaterSurface);
 
+
+		//chart for Temperature data
 		Chart.defaults.global.maintainAspectRatio = false;
-		let chart = document.getElementById("chart").getContext("2d");
-		let TestChart = new Chart(chart, {
+		let chartTemp = document.getElementById("chartTemp").getContext("2d");
+		let Chart1 = new Chart(chartTemp, {
+			//maintainAspectRatio : 'false',
+			responsive: 'true',
+			type: 'line',
+			data: {
+				labels: ["Temperature"],
+				datasets: [{
+					label: '# of Votes',
+					data: [obj[0].Temperature],
+					backgroundColor: [
+						'#009E7F'
+					],
+					borderColor: [
+						'#00846B',
+					],
+					borderWidth: 5
+				}]
+			},
+			options: {
+				scales: {
+					yAxes: [{
+						ticks: {
+							beginAtZero:true
+						}
+					}]
+				}
+			}
+		});
+
+    	//chart for Air Humidity data
+		Chart.defaults.global.maintainAspectRatio = false;
+		let chartAirh = document.getElementById("chartAirh").getContext("2d");
+		let Chart2 = new Chart(chartAirh, {
 			//maintainAspectRatio : 'false',
 			responsive: 'true',
 			type: 'line',
@@ -73,19 +117,122 @@ $(document).ready(function() {
 			}
 		});
     
+   
+		//chart for Soil Humidity data
+		Chart.defaults.global.maintainAspectRatio = false;
+		let chartSoilh = document.getElementById("chartSoilh").getContext("2d");
+		let Chart3 = new Chart(chartSoilh, {
+			//maintainAspectRatio : 'false',
+			responsive: 'true',
+			type: 'line',
+			data: {
+				labels: ["Temperature"],
+				datasets: [{
+					label: '# of Votes',
+					data: [obj[0].Temperature],
+					backgroundColor: [
+						'#009E7F'
+					],
+					borderColor: [
+						'#00846B',
+					],
+					borderWidth: 5
+				}]
+			},
+			options: {
+				scales: {
+					yAxes: [{
+						ticks: {
+							beginAtZero:true
+						}
+					}]
+				}
+			}
+		});
+    	
+
+		//chart for Water Surface data
+		Chart.defaults.global.maintainAspectRatio = false;
+		let chartWater = document.getElementById("chartWater").getContext("2d");
+		let Chart4 = new Chart(chartWater, {
+			//maintainAspectRatio : 'false',
+			responsive: 'true',
+			type: 'line',
+			data: {
+				labels: ["Temperature"],
+				datasets: [{
+					label: '# of Votes',
+					data: [obj[0].Temperature],
+					backgroundColor: [
+						'#009E7F'
+					],
+					borderColor: [
+						'#00846B',
+					],
+					borderWidth: 5
+				}]
+			},
+			options: {
+				scales: {
+					yAxes: [{
+						ticks: {
+							beginAtZero:true
+						}
+					}]
+				}
+			}
+		});
+
+
+
     });
-	
+
 	onoff.click(() => {
 
 		colorChange.toggleClass('-darkmode');
 		heading.toggleClass('-darkmode');
 		nameWrapper.toggleClass('-darkmode');
 		middleWrapper.toggleClass('-darkmode');
-		statsBox.toggleClass('-darkmode');
+		statsBox1.toggleClass('-darkmode');
+		statsBox2.toggleClass('-darkmode');
+		statsBox3.toggleClass('-darkmode');
+		statsBox4.toggleClass('-darkmode');
 		chartWrapper.toggleClass('-darkmode');
 
 	});
 
+	statsBox1.click(() => {
+		console.log("test1");
+		divChart1.css('display','block');
+		divChart2.css('display','none');
+		divChart3.css('display','none');
+		divChart4.css('display','none');
+
+	});
+
+	statsBox2.click(() => {
+		console.log("test2");
+		divChart1.css('display','none');
+		divChart2.css('display','block');
+		divChart3.css('display','none');
+		divChart4.css('display','none');
+	});
+
+	statsBox3.click(() => {
+		console.log("test3");
+		divChart1.css('display','none');
+		divChart2.css('display','none');
+		divChart3.css('display','block');
+		divChart4.css('display','none');
+	});
+
+	statsBox4.click(() => {
+		console.log("test4");
+		divChart1.css('display','none');
+		divChart2.css('display','none');
+		divChart3.css('display','none');
+		divChart4.css('display','block');
+	});
 
   	/*$('#onoffSwitch').click(() => {
 
