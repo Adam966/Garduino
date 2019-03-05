38063cb7aab3ab2fa6d52f3d4d8c5646f45cfa43
tomvarga620 <tomas.varga@akademiasovy.sk>
2019-03-05 22:29:44 +0100

    added function to add divs onclick

--- web/js/floweradd.script.js
+++ web/js/floweradd.script.js
@@ -11,12 +11,20 @@ $(document).ready(() => {
 	const nameWrapper = $('#nameWrapper');
 	const middleWrapper = $('#middleWrapper');
 
+	//divs for add plants
+	const addPlant = $('#fixedBox');
 
 	onoff.click(() => {
 		colorChange.toggleClass('-darkmode');
 		heading.toggleClass('-darkmode');
 		nameWrapper.toggleClass('-darkmode');
 		middleWrapper.toggleClass('-darkmode');
+	});
+
+	//TODO popup na click a pridanie kvetiny až po zadani mena a potvrdení
+	addPlant.click(() => {
+		console.log("test addPlant");
+		middleWrapper.append("<div id='addedPlant'></div>");
 	});
 
 });
